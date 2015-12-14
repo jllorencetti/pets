9c45ab8d0df124465e47e94c2427b4073eeb4048
João Luiz Lorencetti <me@dirtycoder.net>
2015-12-14 19:13:13 -0200

    Display more details about the Owner in the admin list.

--- pets/users/admin.py
+++ pets/users/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+
 from users.models import OwnerProfile
 
 
-admin.site.register(OwnerProfile)
+class OwnerProfileAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'date_joined',
+                    'last_login', 'is_information_confirmed')
+
+
+admin.site.register(OwnerProfile, OwnerProfileAdmin)
