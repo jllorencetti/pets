05a04cf94924070dae8ce63fc2d2486a88a3a87f
Paulo Romano <pauloromanocarvalho@gmail.com>
2016-03-01 20:55:22 -0300

    Base request unsolved cases command test

---
+++ pets/meupet/management/commands/request_unsolvedcases_status.py
@@ -0,0 +1,5 @@
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    pass
--- pets/meupet/tests.py
+++ pets/meupet/tests.py
@@ -2,13 +2,15 @@ import shutil
 import tempfile
 from unittest.mock import MagicMock, patch
 
+from django.core.management import BaseCommand
 from django.core.urlresolvers import reverse
 from django.test import TestCase, override_settings
 from django.utils.timezone import now, timedelta
 
 
 from meupet import forms
-from meupet.management.commands.shareonfacebook import Command
+from meupet.management.commands.shareonfacebook import Command as CmdShareOnFacebook
+from meupet.management.commands.request_unsolvedcases_status import Command as CmdRequestUnsolvedcasesStatus
 from meupet.models import Pet, Kind, Photo, City
 from users.models import OwnerProfile
 
@@ -456,7 +458,7 @@ class ManagementCommandShareOnFacebookTest(TestCase):
         mock.get_renewed_token.return_value = 'token'
         mock.get_attachment.return_value = link
 
-        cmd = Command()
+        cmd = CmdShareOnFacebook()
 
         cmd.get_attachment = mock.get_attachment
         cmd.get_renewed_token = mock.get_renewed_token
@@ -464,6 +466,33 @@ class ManagementCommandShareOnFacebookTest(TestCase):
         with patch('facebook.GraphAPI.put_wall_post') as mock:
             cmd.handle()
             return mock
+
+
+class ManagementCommandRequestUnsolvedCases(TestCase):
+    def setUp(self):
+        self.admin = OwnerProfile.objects.create_user(username='admin', password='admin', email='pets.adim@mailinator.com')
+        today = now()
+        today_2months_ago = now() - timedelta(days=60)
+        today_4months_ago = now() - timedelta(days=120)
+        self.create_pet('Cat', 'Pet1', Pet.MISSING, modified=today)
+        self.create_pet('Dog', 'Pet2', Pet.FOR_ADOPTION, modified=today_2months_ago)
+        self.create_pet('Cat', 'Pet3', Pet.MISSING, modified=today_4months_ago)
+        self.create_pet('Dog', 'Pet4', Pet.FOR_ADOPTION, modified=today_4months_ago)
+
+    def create_pet(self, kind, name='Pet', status=Pet.MISSING, **kwargs):
+        image = get_test_image_file()
+        user = self.admin
+        kind = Kind.objects.get_or_create(kind=kind)[0]
+        return Pet.objects.create(name='Testing ' + name, description='Bla',
+                                  profile_picture=image, owner=user, kind=kind,
+                                  status=status, **kwargs)
+
+    def test_cmd_request_unsolvedcases_status(self):
+        """Must have a management cmd to request unsolved cases"""
+        cmd = CmdRequestUnsolvedcasesStatus()
+        self.assertIsInstance(cmd, BaseCommand)
+
+
 
 
 @override_settings(MEDIA_ROOT=MEDIA_ROOT)
