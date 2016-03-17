8fc431268a4ebb77f57dca94cd6829795da7fc4d
João Luiz Lorencetti <me@dirtycoder.net>
2016-03-16 23:02:29 -0300

    Fix #14 meupet regex was too hungry

---
+++ pets/meupet/tests/test_kind_view.py
@@ -0,0 +1,38 @@
+from django.shortcuts import resolve_url
+from django.test import TestCase
+
+from meupet.models import Kind
+
+
+class LostKindView(TestCase):
+    def setUp(self):
+        self.kind = Kind.objects.create(kind='Kind')
+
+    def test_get(self):
+        resp = self.client.get(resolve_url('meupet:lost', self.kind.id))
+
+        self.assertEqual(200, resp.status_code)
+        self.assertTemplateUsed(resp, 'meupet/pet_list.html')
+
+    def test_get_fallback(self):
+        resp = self.client.get('/pets/lost/{}/'.format(self.kind.id))
+
+        self.assertEqual(200, resp.status_code)
+        self.assertTemplateUsed(resp, 'meupet/pet_list.html')
+
+
+class AdoptionKindView(TestCase):
+    def setUp(self):
+        self.kind = Kind.objects.create(kind='Kind')
+
+    def test_get(self):
+        resp = self.client.get(resolve_url('meupet:adoption', self.kind.id))
+
+        self.assertEqual(200, resp.status_code)
+        self.assertTemplateUsed(resp, 'meupet/pet_list.html')
+
+    def test_get_fallback(self):
+        resp = self.client.get('/pets/adoption/{}/'.format(self.kind.id))
+
+        self.assertEqual(200, resp.status_code)
+        self.assertTemplateUsed(resp, 'meupet/pet_list.html')
--- pets/meupet/urls.py
+++ pets/meupet/urls.py
@@ -11,13 +11,13 @@ urlpatterns = patterns(
     url(r'^para-adocao/(?P<id>[0-9]+)/$', views.AdoptionPetView.as_view(), name='adoption'),
     url(r'^busca-rapida/$', views.QuickSearchView.as_view(), name='quick_search'),
     url(r'^busca/$', views.SearchView.as_view(), name='search'),
-    url(r'^pet/(?P<slug>.+)/foto/$', views.upload_image, name='upload_image'),
-    url(r'^(?P<slug>.+)/editar/$', views.EditPetView.as_view(), name='edit'),
-    url(r'^(?P<slug>.+)/poster/$', views.poster, name='poster'),
-    url(r'^(?P<slug>.+)/editar/situacao/$', views.change_status, name='change_status'),
-    url(r'^(?P<slug>.+)/deletar/$', views.delete_pet, name='delete_pet'),
-    url(r'^(?P<slug>.+)/registrado/$', views.registered, name='registered'),
-    url(r'^(?P<pk_or_slug>.+)/$', views.pet_detail_view, name='detail'),
+    url(r'^pet/(?P<slug>[-\w]*)/foto/$', views.upload_image, name='upload_image'),
+    url(r'^(?P<slug>[-\w]*)/editar/$', views.EditPetView.as_view(), name='edit'),
+    url(r'^(?P<slug>[-\w]*)/poster/$', views.poster, name='poster'),
+    url(r'^(?P<slug>[-\w]*)/editar/situacao/$', views.change_status, name='change_status'),
+    url(r'^(?P<slug>[-\w]*)/deletar/$', views.delete_pet, name='delete_pet'),
+    url(r'^(?P<slug>[-\w]*)/registrado/$', views.registered, name='registered'),
+    url(r'^(?P<pk_or_slug>[-\w]*)/$', views.pet_detail_view, name='detail'),
 
     # I'll keep this here for compatibility with links shared
     # on Facebook.
