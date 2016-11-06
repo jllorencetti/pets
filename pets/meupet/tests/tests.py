952d51881a35b6a1c583f2f635a5add278bb89e7
João Luiz Lorencetti <me@dirtycoder.net>
2016-11-06 11:54:49 -0200

    Add model_mommy to 'meupet' app's tests, this should facilitate smaller, cleaner tests when I come back to refactor it properly; Add doc strings to 'meupet' app's tests

--- pets/meupet/tests/tests.py
+++ pets/meupet/tests/tests.py
@@ -3,6 +3,9 @@ import tempfile
 
 from django.core.urlresolvers import reverse
 from django.test import TestCase, override_settings
+from django.utils.text import slugify
+
+from model_mommy import mommy
 
 from meupet import forms
 from meupet.models import Pet, Kind, City
@@ -15,7 +18,11 @@ MEDIA_ROOT = tempfile.mkdtemp()
 @override_settings(MEDIA_ROOT=MEDIA_ROOT)
 class MeuPetTestCase(TestCase):
     def setUp(self):
-        self.admin = OwnerProfile.objects.create_user(username='admin', password='admin')
+        self.admin = OwnerProfile.objects.create_user(
+            username='admin',
+            password='admin',
+            facebook='http://www.facebook.com/owner_profile'
+        )
         self.test_city, _ = City.objects.get_or_create(city='Testing City')
 
     @staticmethod
@@ -24,13 +31,21 @@ class MeuPetTestCase(TestCase):
         file = tempfile.NamedTemporaryFile(suffix='.png')
         return ImageFile(file, name=file.name)
 
-    def create_pet(self, kind, name='Pet', status=Pet.MISSING, **kwargs):
-        image = self.get_test_image_file()
-        user = self.admin
-        kind = Kind.objects.get_or_create(kind=kind)[0]
-        return Pet.objects.create(name='Testing ' + name, description='Bla',
-                                  profile_picture=image, owner=user, kind=kind,
-                                  status=status, **kwargs)
+    def create_pet(self, status=Pet.MISSING, kind=None, **kwargs):
+        pet = mommy.make(Pet, status=status, owner=self.admin, **kwargs)
+
+        if kind:
+            kind, _ = Kind.objects.get_or_create(kind=kind, slug=slugify(kind))
+
+            if kwargs.get('_quantity', None):
+                for p in pet:
+                    p.kind = kind
+                    p.save()
+            else:
+                pet.kind = kind
+                pet.save()
+
+        return pet
 
     @classmethod
     def tearDownClass(cls):
@@ -40,49 +55,53 @@ class MeuPetTestCase(TestCase):
 
 class MeuPetTest(MeuPetTestCase):
     def test_titleize_name(self):
-        data = {
-            'name': 'TESTING NAME'
-        }
+        """Force the case of the pet's name to be titleized"""
+        data = {'name': 'TESTING NAME'}
         form = forms.PetForm(data=data)
         form.is_valid()
         self.assertEquals(form.cleaned_data['name'], 'Testing Name')
 
     def test_display_all_pets(self):
-        self.create_pet('Goat', 'Goat')
-        self.create_pet('Cat', 'Cat')
+        """Display recently adds pets in the index page"""
+        first_pet = self.create_pet()
+        second_pet = self.create_pet()
 
         home = self.client.get(reverse('meupet:index'))
 
-        self.assertContains(home, 'Testing Goat')
-        self.assertContains(home, 'Testing Cat')
+        self.assertContains(home, first_pet.name)
+        self.assertContains(home, second_pet.name)
 
     def test_display_kinds_sidebar(self):
+        """The side bar should show only kinds that have pets registered"""
         Kind.objects.get_or_create(kind='0 Pets')
-        self.create_pet('Goat')
-        self.create_pet('Cat')
+        first_pet = self.create_pet(kind='Cat')
+        second_pet = self.create_pet(kind='Dog')
 
         home = self.client.get(reverse('meupet:index'))
 
-        self.assertContains(home, 'Goat')
-        self.assertContains(home, 'Cat')
+        self.assertContains(home, first_pet.kind.kind)
+        self.assertContains(home, second_pet.kind.kind)
         self.assertNotContains(home, '0 Pets')
 
     def test_display_only_pets_from_kind(self):
-        self.create_pet('Goat', 'Goat')
-        self.create_pet('Cat', 'Cat')
-        self.create_pet('Cat', 'Costela')
+        """Only display the pets from the kind being shown"""
+        first_cat = self.create_pet(kind='Cat')
+        second_cat = self.create_pet(kind='Cat')
+        dog = self.create_pet(kind='Dog')
+
         kind = Kind.objects.get(kind='Cat')
 
         content = self.client.get(reverse('meupet:lost', args=[kind.id]))
         pets_count = Pet.objects.filter(kind=kind).count()
 
-        self.assertContains(content, 'Testing Cat')
-        self.assertContains(content, 'Testing Costela')
-        self.assertNotContains(content, 'Testing Goat')
+        self.assertContains(content, first_cat.name)
+        self.assertContains(content, second_cat.name)
+        self.assertNotContains(content, dog.name)
         self.assertEqual(2, pets_count)
 
     def test_show_edit_button_for_own_if_logged_pet(self):
-        pet = self.create_pet('Own Pet')
+        """Show the edit button only if the owner is logged in"""
+        pet = self.create_pet()
         self.client.login(username='admin', password='admin')
 
         response = self.client.get(pet.get_absolute_url())
@@ -91,75 +110,73 @@ class MeuPetTest(MeuPetTestCase):
         self.assertContains(response, reverse('meupet:edit', args=[pet.slug]))
 
     def test_load_data_for_editing_pet(self):
-        pet = self.create_pet('Own Pet', 'Own Pet')
+        """Assert that the saved data is being preloaded in the edit page"""
+        pet = self.create_pet()
         self.client.login(username='admin', password='admin')
 
         response = self.client.get(reverse('meupet:edit', args=[pet.slug]))
 
         self.assertTemplateUsed(response, 'meupet/edit.html')
-        self.assertContains(response, 'Testing Own Pet')
-        self.assertContains(response, 'Bla')
+        self.assertContains(response, pet.name)
+        self.assertContains(response, pet.description)
         self.assertContains(response, 'Salvar Alterações')
 
     def test_owner_can_delete_pet(self):
-        pet = self.create_pet('Own Pet')
+        """Owner can delete it's own pets"""
+        pet = self.create_pet()
         self.client.login(username='admin', password='admin')
 
         response = self.client.post(reverse('meupet:delete_pet', args=[pet.slug]), follow=True)
 
         self.assertTemplateUsed(response, 'meupet/index.html')
-        self.assertNotContains(response, 'Testing Pet')
+        self.assertNotContains(response, pet.name)
 
-    def test_other_user_can_delete_pet(self):
-        pet = self.create_pet('Own Pet')
+    def test_user_cant_delete_pet_from_other(self):
+        """Assert that an user can't delete pets from other people"""
+        pet = self.create_pet()
         OwnerProfile.objects.create_user(username='other', password='user')
         self.client.login(username='other', password='user')
 
         response = self.client.post(reverse('meupet:delete_pet', args=[pet.slug]), follow=True)
 
         self.assertTemplateUsed(response, 'meupet/pet_detail.html')
-        self.assertContains(response, 'Testing Pet')
+        self.assertContains(response, pet.name)
 
     def test_can_edit_pet(self):
-        pet = self.create_pet('Own Pet')
-        kind = Kind.objects.first()
+        """Pet's owner can edit it's own pet"""
         self.client.login(username='admin', password='admin')
-        url = Pet.objects.first().profile_picture.url
+        pet = self.create_pet(kind='Cat')
 
         response_post = self.client.post(reverse('meupet:edit', args=[pet.slug]),
                                          data={'name': 'Testing Fuzzy Boots',
                                                'description': 'My lovely cat',
                                                'city': self.test_city.id,
-                                               'kind': kind.id,
+                                               'kind': pet.kind.id,
                                                'status': pet.status,
-                                               'profile_picture': url})
+                                               'profile_picture': pet.profile_picture.url})
         response_get = self.client.get(pet.get_absolute_url())
 
         self.assertRedirects(response_post, pet.get_absolute_url())
         self.assertContains(response_get, 'Testing Fuzzy Boots')
 
-    def test_show_facebook_only_if_registered(self):
-        self.create_pet('Own Pet', 'Own Pet')
-        self.create_pet('Second Pet', 'Second Pet')
-        user = OwnerProfile.objects.create_user(username='second_user', password='admin')
-        user.facebook = 'http://www.facebook.com/owner_profile'
-        user.save()
-        pet = Pet.objects.first()
-        pet.owner = user
-        pet.save()
+    def test_show_facebook_link(self):
+        """Displays a link to the Facebook profile of the owner in the of the pet's details"""
+        pet = self.create_pet()
 
         resp_with_facebook = self.client.get(pet.get_absolute_url())
 
         self.assertContains(resp_with_facebook, 'http://www.facebook.com/owner_profile')
 
     def test_show_link_for_owner_profile(self):
-        pet = self.create_pet('Pet')
+        """Displays a link to the profile of the owner"""
+        pet = self.create_pet()
 
         response = self.client.get(pet.get_absolute_url())
 
         self.assertContains(response, reverse('users:user_profile', args=[self.admin.id]))
 
     def test_should_redirect_if_not_confirmed(self):
+        """Don't allow users to register a pet if their info are not confirmed"""
         self.client.login(username='admin', password='admin')
 
         response = self.client.get(reverse('meupet:register'))
@@ -167,9 +184,9 @@ class MeuPetTest(MeuPetTestCase):
         self.assertRedirects(response, '/user/profile/edit/')
 
     def test_should_access_if_confirmed(self):
-        admin = OwnerProfile.objects.first()
-        admin.is_information_confirmed = True
-        admin.save()
+        """Allow user to access the pet register page if the information is confirmed"""
+        self.admin.is_information_confirmed = True
+        self.admin.save()
         self.client.login(username='admin', password='admin')
 
         response = self.client.get(reverse('meupet:register'))
@@ -177,8 +194,9 @@ class MeuPetTest(MeuPetTestCase):
         self.assertTemplateUsed(response, 'meupet/register_pet.html')
 
     def test_only_owner_can_see_edit_page(self):
+        """Do not show the edit page if the logged user is not the owner"""
         OwnerProfile.objects.create_user(username='Other User', password='otherpass')
-        pet = self.create_pet('Own Pet')
+        pet = self.create_pet()
         self.client.login(username='Other User', password='otherpass')
 
         response = self.client.get(reverse('meupet:edit', args=[pet.slug]))
@@ -186,36 +204,51 @@ class MeuPetTest(MeuPetTestCase):
         self.assertRedirects(response, pet.get_absolute_url())
 
     def test_display_status_on_pet_page(self):
-        missing_pet = self.create_pet('Test', 'Test 1')
-        adoption_pet = self.create_pet('Test', 'Test 2', status=Pet.FOR_ADOPTION)
+        """Show the name of the pet and the readable status name"""
+        missing_pet = self.create_pet()
+        adoption_pet = self.create_pet(status=Pet.FOR_ADOPTION)
 
         response_missing = self.client.get(missing_pet.get_absolute_url())
         response_adoption = self.client.get(adoption_pet.get_absolute_url())
 
-        self.assertContains(response_missing, 'Test 1 - Desaparecido')
-        self.assertContains(response_adoption, 'Test 2 - Para Adoção')
+        self.assertContains(response_missing, '{0} - {1}'.format(
+            missing_pet.name,
+            missing_pet.get_status_display()
+        ))
+
+        self.assertContains(response_adoption, '{0} - {1}'.format(
+            adoption_pet.name,
+            adoption_pet.get_status_display()
+        ))
 
     def test_manager_lost_found(self):
-        missing_pet = self.create_pet('Test')
-        self.create_pet('Test')
-        self.create_pet('Test', status=Pet.FOUND)
-        self.create_pet('Test', status=Pet.FOR_ADOPTION)
+        """Manager should return lost and found pets of the correct kind"""
+        self.create_pet(kind='Dog', _quantity=2)
+        self.create_pet(kind='Dog', status=Pet.FOUND)
+        self.create_pet(kind='Dog', status=Pet.FOR_ADOPTION)
 
-        pets = Pet.objects.get_lost_or_found(missing_pet.kind.id)
+        dog_kind = Kind.objects.get(kind='Dog')
+
+        pets = Pet.objects.get_lost_or_found(dog_kind.id)
 
         self.assertEquals(len(pets), 3)
 
     def test_manager_adoption_adopted(self):
-        adopted_pet = self.create_pet('Test', status=Pet.ADOPTED)
-        self.create_pet('Test')
-        self.create_pet('Test', status=Pet.FOUND)
-        self.create_pet('Test', status=Pet.FOR_ADOPTION)
+        """Manager should return adopted and for adoption pets of the correct kind"""
+        self.create_pet(kind='Test')
+        self.create_pet(kind='Test', status=Pet.FOUND)
+        self.create_pet(kind='Test', status=Pet.ADOPTED)
+        self.create_pet(kind='Test', status=Pet.FOR_ADOPTION)
 
-        pets = Pet.objects.get_for_adoption_adopted(adopted_pet.kind.id)
+        kind = Kind.objects.get(kind='Test')
+
+        pets = Pet.objects.get_for_adoption_adopted(kind.id)
 
         self.assertEquals(len(pets), 2)
 
     def test_incorrect_form_submission_reload_page_with_values(self):
+        """Incomplete form submission should reload the page
+        preserving the submitted information"""
         self.client.login(username='admin', password='admin')
 
         response = self.client.post(reverse('meupet:register'),
@@ -225,7 +258,8 @@ class MeuPetTest(MeuPetTestCase):
         self.assertContains(response, 'Test Description')
 
     def test_show_add_photo_button_in_pet_page_owner_logged_in(self):
-        pet = self.create_pet('Cat')
+        """Display button to add more photos to the pet profile"""
+        pet = self.create_pet()
         self.client.login(username='admin', password='admin')
 
         response = self.client.get(pet.get_absolute_url())
@@ -234,28 +268,31 @@ class MeuPetTest(MeuPetTestCase):
         self.assertContains(response, 'another_picture')
 
     def test_show_city(self):
-        pet = self.create_pet('Cat')
-        pet.city = self.test_city
-        pet.save()
+        """Display the name of the city where the pet belongs"""
+        pet = self.create_pet(city=self.test_city)
 
         response = self.client.get(pet.get_absolute_url())
 
         self.assertContains(response, self.test_city)
 
     def test_show_size(self):
-        pet = self.create_pet('Cat', size=Pet.SMALL)
+        """Display the human readable size of the pet"""
+        pet = self.create_pet(size=Pet.SMALL)
 
         response = self.client.get(pet.get_absolute_url())
 
         self.assertContains(response, 'Pequeno')
 
-    def test_custom_search_without_filters(self):
+    def test_search_without_filters(self):
+        """Show message informing the user that she needs to use at least
+        one filter to be able to search"""
         response = self.client.post(reverse('meupet:search'), {})
 
         self.assertContains(response, 'É necessário selecionar ao menos um filtro')
 
-    def test_custom_search_with_filter(self):
-        pet = self.create_pet('Dog', city=self.test_city)
+    def test_search_with_filter(self):
+        """Search by city should show the pet"""
+        pet = self.create_pet(city=self.test_city)
 
         response = self.client.post(reverse('meupet:search'), {'city': self.test_city.id}, follow=True)
 
@@ -263,14 +300,16 @@ class MeuPetTest(MeuPetTestCase):
         self.assertContains(response, pet.city)
 
     def test_show_pet_sex(self):
-        pet = self.create_pet('Dog', sex=Pet.FEMALE)
+        """Display the human readable sex of the pet"""
+        pet = self.create_pet(sex=Pet.FEMALE)
 
         response = self.client.get(pet.get_absolute_url())
 
         self.assertContains(response, 'Fêmea')
 
     def test_change_status_and_show_status_label(self):
-        pet = self.create_pet('Dog', status=Pet.FOR_ADOPTION)
+        """Updates status of the pet from 'For Adoption' to 'Adopted'"""
+        pet = self.create_pet(status=Pet.FOR_ADOPTION)
         self.client.post(reverse('meupet:change_status', args=[pet.slug]))
 
         response = self.client.get(reverse('meupet:index'))
@@ -278,37 +317,36 @@ class MeuPetTest(MeuPetTestCase):
         self.assertContains(response, '<h2 class="text-center"><span>Adotado! :)</span></h2>')
 
     def test_get_pets_unpublished(self):
-        pet = self.create_pet('Dog')
+        """Manager method should return pets not published on Facebook yet"""
+        pet = self.create_pet()
+        self.create_pet(published=True, _quantity=3)
 
         pets = Pet.objects.get_unpublished_pets()
 
         self.assertIn(pet, pets)
-
-    def test_dont_get_published_pet(self):
-        pet = self.create_pet('Dog', published=True)
-
-        pets = Pet.objects.get_unpublished_pets()
-
-        self.assertNotIn(pet, pets)
+        self.assertEqual(pets.count(), 1)
 
 
 class PaginationListPetViewTest(MeuPetTestCase):
     def setUp(self):
         super(PaginationListPetViewTest, self).setUp()
-        self.pet = self.create_pet('First Kind')
+        self.pet = self.create_pet(kind='First Kind')
 
     def test_get_page_query_string(self):
+        """Should return the page informed in the query string"""
         resp = self.client.get(reverse('meupet:lost', args=[self.pet.kind_id]), {'page': 1})
 
-        self.assertContains(resp, 'Testing Pet')
+        self.assertContains(resp, self.pet.name)
 
     def test_page_not_integer(self):
+        """In case of a non-integer page, i.e, incorrent url, we should show the first page"""
         pets, _ = paginate_pets(Pet.objects.all(), 'page')
 
         self.assertEqual(1, len(pets))
         self.assertEqual('First Kind', pets[0].kind.kind)
 
     def test_empty_page(self):
+        """If the user inform an empty page we should present the last page with data"""
         pets, _ = paginate_pets(Pet.objects.all(), 42)
 
         self.assertEqual(1, len(pets))
--- pets/pets/settings/dev.py
+++ pets/pets/settings/dev.py
@@ -1,5 +1,11 @@
+from model_mommy import generators
+
 from .prod import *
 
 DEBUG = True
 
 INSTALLED_APPS += ('debug_toolbar', 'test_without_migrations')
+
+MOMMY_CUSTOM_FIELDS_GEN = {
+    'autoslug.fields.AutoSlugField': generators.gen_slug,
+}
