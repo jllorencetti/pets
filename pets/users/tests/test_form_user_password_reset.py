6374fea939a481438ed2c95c7374a6aa802db37d
João Luiz Lorencetti <me@dirtycoder.net>
2016-11-14 18:46:50 -0200

    Adds test for the users password reset form

---
+++ pets/users/tests/test_form_user_password_reset.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+from users.forms import UsersPasswordResetForm
+
+
+class UsersPasswordResetFormTest(TestCase):
+    def test_submit_button_present(self):
+        """Submit button should be present in the helper"""
+        form = UsersPasswordResetForm(user=None)
+
+        self.assertIn('Recuperar minha senha', form.helper.inputs[0].value)
