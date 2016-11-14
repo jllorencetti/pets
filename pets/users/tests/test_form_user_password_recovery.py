7256d4eb88f316080f7750f8a024a19847b65f7b
João Luiz Lorencetti <me@dirtycoder.net>
2016-11-14 17:49:41 -0200

    Adds a couple of tests for the password recovery form

---
+++ pets/users/tests/test_form_user_password_recovery.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+from users.forms import UsersPasswordRecoveryForm
+
+
+class UsersPasswordRecoveryFormTest(TestCase):
+    def test_username_email_label(self):
+        """Label should be corrected configured"""
+        form = UsersPasswordRecoveryForm()
+
+        self.assertEqual(form['username_or_email'].label, '')
+
+    def test_submit_button_present(self):
+        """Submit button should be present in the helper"""
+        form = UsersPasswordRecoveryForm()
+
+        self.assertIn('Recuperar minha senha', form.helper.inputs[0].value)
