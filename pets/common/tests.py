01ddd0eef9d3b41b0c3c92e1f2a1d0a190505f24
João Luiz Lorencetti <me@dirtycoder.net>
2017-04-06 20:01:20 -0300

    Creates a simple util function to clear text

--- pets/common/tests.py
+++ pets/common/tests.py
@@ -1,1 +1,13 @@
-# Create your tests here.
+from django.test import TestCase
+
+from common import utils
+
+
+class UtilsTest(TestCase):
+    def test_clear_text(self):
+        """Should remove combining marks from text"""
+        sp = utils.clear_text('São Paulo')
+        rand = utils.clear_text('ç~ã`é´â^ô')
+
+        self.assertEqual('Sao Paulo', sp)
+        self.assertEqual('caeao', rand)
