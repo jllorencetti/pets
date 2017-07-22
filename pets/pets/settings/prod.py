9509258e85d282988a3011cd031ce893d4ee1da5
João Luiz Lorencetti <me@dirtycoder.net>
2017-07-22 16:42:36 -0300

    Fix social auth pipeline

--- pets/pets/settings/prod.py
+++ pets/pets/settings/prod.py
@@ -104,16 +104,16 @@ AUTHENTICATION_BACKENDS = (
 )
 
 SOCIAL_AUTH_PIPELINE = (
-    'social_django.pipeline.social_auth.social_details',
-    'social_django.pipeline.social_auth.social_uid',
-    'social_django.pipeline.social_auth.auth_allowed',
-    'social_django.pipeline.social_auth.social_user',
-    'social_django.pipeline.user.get_username',
-    'social_django.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
     'users.pipeline.add_facebook_link',
-    'social_django.pipeline.social_auth.associate_user',
-    'social_django.pipeline.social_auth.load_extra_data',
-    'social_django.pipeline.user.user_details',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
 )
 
 ROOT_URLCONF = 'pets.urls'
