e44639219700c5479261ae2adc23abffd7ba3af2
João Luiz Lorencetti <jll.linux@gmail.com>
2018-03-03 12:05:47 -0300

    add filters to users admin page

--- pets/users/admin.py
+++ pets/users/admin.py
@@ -4,8 +4,19 @@ from users.models import OwnerProfile
 
 
 class OwnerProfileAdmin(admin.ModelAdmin):
-    list_display = ('username', 'first_name', 'last_name', 'date_joined',
-                    'last_login', 'is_information_confirmed')
+    list_display = (
+        'username',
+        'first_name',
+        'last_name',
+        'date_joined',
+        'last_login',
+        'is_information_confirmed',
+    )
+    list_filter = (
+        'date_joined',
+        'last_login',
+        'is_information_confirmed'
+    )
 
 
 admin.site.register(OwnerProfile, OwnerProfileAdmin)
