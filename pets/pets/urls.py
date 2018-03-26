2998a7e17d29f558137a244a533e14b14ef61da2
João Luiz Lorencetti <jll.linux@gmail.com>
2018-03-26 19:24:07 -0300

    remove debug_toolbar import

--- pets/pets/urls.py
+++ pets/pets/urls.py
@@ -13,6 +13,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [url(r'^__debug__/', include(debug_toolbar.urls))]
     urlpatterns += static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
